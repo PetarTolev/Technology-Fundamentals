1d71f9dc9aff5ce6eb7c0fbee1a38ebdfa2d2c4c
PetarTolev <petar.dtolev@gmail.com>
2019-08-10 08:45:52 +0300

    Added exercise

---
+++ (Lab) C# Basic Web Phonebook/Phonebook/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Phonebook.Data.Models;
+using Phonebook.Data;
+
+namespace Phonebook.Controllers
+{
+    public class ContactController : Controller
+    {
+        [HttpPost]
+        public IActionResult Create (Contact contact)
+        {
+            DataAccess.Contacts.Add(contact);
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
